989b233dd135b5beb7b8c8957182b6e9680c52ef
Yiannis Patrikiadis <yiannis@arion.gr>
2023-05-08 16:48:54 +0300

    double line to category buttons

--- DestoPesto/DestoPesto/Services/JsonHandler.cs
+++ DestoPesto/DestoPesto/Services/JsonHandler.cs
@@ -335,6 +335,7 @@ namespace DestoPesto.Services
                 var profiles = Connectivity.ConnectionProfiles;
                 //if(profiles.Contains(ConnectionProfile.WiFi))
                 _Uri = "http://10.0.0.13:5005/";
+                _Uri = "http://10.0.0.10:5005/";
 #endif
 
             }
