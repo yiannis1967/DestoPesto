f47d14cfe54a49b18112d831ffb948293b404388
Yiannis Patrikiadis <yiannis@arion.gr>
2024-06-20 13:13:23 +0300

    bugs

--- Authentication.Android/Resources/Resource.designer.cs
+++ Authentication.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace Authentication.Android
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.99")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.2.120")]
 	public partial class Resource
 	{
 		
